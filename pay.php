de101ea6620e592554f5e253f13fe5845184d196
vijai <vijai.dj@gmail.com>
2019-05-25 00:26:00 +0530

    strip

---
+++ pay.php
@@ -0,0 +1,11 @@
+<form action="http://localhost/ticketforus/stripe-payment.php" method="POST">
+  <script
+    src="https://checkout.stripe.com/checkout.js"
+    class="stripe-button"
+    data-key="pk_test_wosNc22PGHfCAjfT9UJTawqT00NnmVJayT"
+    data-name="2 Tickets"
+    data-description="Ticket for US"
+    data-amount="50.00"
+    data-currency="EUR">
+  </script>
+</form>
